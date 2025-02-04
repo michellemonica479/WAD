270a854a81d91968a550f1aae624d6a71eb5f354
michellemonica479 <monixmichellemm@gmail.com>
2025-02-04 12:41:44 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,57 @@
+let x = 20;
+let name ="Gift";
+console. log(name);
+
+let age = 21;
+let isStudent = true;
+let isOnline = false;
+let isMale = false;
+
+//Number
+const price = 1000;
+
+//Null
+let y = null;
+
+//Undefined
+let z;
+
+//Object
+let person = {
+    name:'Gift',
+    age:21,
+    grade:'1st class',
+    status:isStudent,
+};
+console.log(person);
+
+//Regular Arrays
+let students = ['gift','shei','kathie'];
+console.log(students [1]);
+
+//Pop-pop()
+let newStudent = students.pop();
+console.log(students);
+
+//push
+students.push('musa','michelle');
+console.log(students);
+
+//shift()
+students.shift();
+console.log(students);
+
+//unshift()
+students.unshift('leah');
+console.log(students);
+
+//slice()
+students.slice();
+console.log(students);
+
+//associative arrays
+let items = {
+    "fruits":'apple',
+    "vegetables":"nakati"
+};
+console.log(items.fruits);
